0bb698a592691cf9d25bce90c103327613f0e4e8
Parkseojin <seojin5565@gmail.com>
2024-03-22 18:49:58 +0900

    [add] : 프로그래머스_체육복

---
+++ src/greedy/Solution_체육복.java
@@ -0,0 +1,43 @@
+package greedy;
+
+import java.util.*;
+
+public class Solution_체육복 {
+  public int solution(int n, int[] lost, int[] reserve) {
+
+    Arrays.sort(lost);
+    Arrays.sort(reserve);
+
+    List<Integer> lostList = new ArrayList<>();
+    List<Integer> reserveList = new ArrayList<>();
+    List<Integer> removeList = new ArrayList<>();
+
+    for (int i : lost) {
+      lostList.add(i);
+    }
+
+    for (int i : reserve) {
+      reserveList.add(i);
+    }
+
+    for (int i : reserveList){
+      if (lostList.contains(i)){
+        removeList.add(i);
+      }
+    }
+
+    reserveList.removeAll(removeList);
+    lostList.removeAll(removeList);
+
+    for(int i : reserveList) {
+      if (lostList.contains(i - 1)){
+        lostList.remove(Integer.valueOf(i - 1));
+      } else if (lostList.contains(i + 1)) {
+        lostList.remove(Integer.valueOf(i + 1));
+      }
+
+    }
+
+    return n - lostList.size();
+  }
+}
