f03216133bbbb0811ae3f7d918e2f451700680bc
Parkseojin <seojin5565@gmail.com>
2024-03-27 17:08:22 +0900

    [add] : 백준_17262

--- src/dp/SJ/Main17626.java
+++ src/dp/SJ/Main17626.java
@@ -1,61 +1,31 @@
 package dp.SJ;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main17626 {
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int N = Integer.parseInt(br.readLine()); // 5000까지
-
-    System.out.println(dp(N));
-
-    System.out.println(Math.sqrt(11339));
-    System.out.println(106 * 106);
-    System.out.println(11339 - 106 * 106);
-
-
+    int N = Integer.parseInt(br.readLine());
+     System.out.println(dp(N));
   }
 
-  // dp 버전
   public static int dp (int N) {
-    int[] dp = new int[5001];
+    int[] dp = new int[50001];
 
-    int INF = 50000;
+    int INF = 500001;
 
-    for (int i = 1; i < dp.length; i++) {
-      dp[i] = INF;
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= N; i++) {
+      double min_value = INF;
+      for (int k = 1; k * k <= i; k++) {
+        min_value = Math.min(min_value, dp[i - k * k]);
+      }
+      dp[i] = (int)min_value + 1;
     }
-
-    dp[3] = 1;
-
-    for(int i = 5; i <= 5000; i++) {
-      dp[i] = Math.min(Math.min(dp[i], dp[i - 3] + 1), dp[i - 5] + 1);
-    }
-
-    dp[N] = (dp[N] == INF) ? -1 : dp[N];
 
     return dp[N];
-  }
-
-  // dp 아닌 버전
-  public static int calc(int N) {
-    int cnt = 0;
-
-    while (N > 0) {
-
-      if (N % 5 == 0) {
-        return cnt + N / 5;
-      } else {
-        if (N < 3) {
-          return -1;
-        } else {
-          N -= 3;
-          cnt++;
-        }
-      }
-    }
-    return cnt;
   }
 }
